19e3c3ec8a82c047b69a0ac81f8f5951d8795baa
Ivan <ivan.01.arabadzhiev@gmail.com>
2022-09-25 20:57:09 +0100

    Update median.py

--- median.py
+++ median.py
@@ -1,5 +1,14 @@
 """Median calculator."""
 
+def median(numbers):
+    if len(numbers) % 2 == 0:
+        var = len(numbers) // 2
+        val_1 = numbers[var]
+        val_2 = numbers[var - 1]
+        return (val_1 + val_2) / 2
+    else:
+        return numbers[len(numbers) // 2]
+
 """ENTER YOUR SOLUTION HERE!"""
 
 while True:
@@ -10,4 +19,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median(numbers))
