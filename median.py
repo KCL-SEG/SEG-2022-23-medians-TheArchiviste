0a14e7b4af74f587ca8ef319e2b1cfad78bd4f13
Ivan <ivan.01.arabadzhiev@gmail.com>
2022-09-27 16:43:06 +0100

    Update median.py

--- median.py
+++ median.py
@@ -1,7 +1,6 @@
 """Median calculator."""
 
 def median(numbers):
-    numbers.sort()
     if len(numbers) % 2 == 0:
         var = len(numbers) // 2
         val_1 = numbers[var]
@@ -15,7 +14,8 @@ def median(numbers):
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")].sort()
+        numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
